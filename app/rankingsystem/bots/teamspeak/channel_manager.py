b9e49bd9b6aed5058585c499e54854c6f502c9d4
Lukas Roth <mail@lukas-roth.dev>
2025-05-26 22:01:42 +0200

    Fixing more permissions

--- app/rankingsystem/bots/teamspeak/channel_manager.py
+++ app/rankingsystem/bots/teamspeak/channel_manager.py
@@ -36,7 +36,7 @@ class ChannelManager:
                 permissions = [
                     ("i_channel_needed_modify_power", 75),  
                     ("i_channel_needed_delete_power", 75),  
-                    ("b_channel_modify_make_default", 1),   
+                    ("b_channel_modify_make_default", 0),   
                     ("b_channel_modify_name", 1),           
                     ("b_channel_modify_topic", 1),          
                     ("b_channel_modify_description", 1),    
@@ -45,7 +45,9 @@ class ChannelManager:
                     ("b_channel_modify_codec_quality", 1),  
                     ("b_channel_modify_codec_latency_factor", 1), 
                     ("b_channel_modify_needed_talk_power", 1), 
-                    ("i_channel_modify_power", 75),        
+                    ("b_channel_modify_make_temporary", 0),
+                    ("i_channel_modify_power", 75),
+                            
                 ]
                 
                 for perm_name, perm_value in permissions:
